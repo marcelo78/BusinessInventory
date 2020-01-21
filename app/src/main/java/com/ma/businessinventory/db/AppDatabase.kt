064dba59496a6c5ac09d239aeb12aa2e0dbabd54
Marcelo <marcelo.agudelo@gmail.com>
2020-01-21 13:28:51 -0500

    Add DataBase

--- app/src/main/java/com/ma/businessinventory/db/AppDatabase.kt
+++ app/src/main/java/com/ma/businessinventory/db/AppDatabase.kt
@@ -1,33 +1,77 @@
 package com.ma.businessinventory.db
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.ma.businessinventory.db.dao.ProductDao
 import com.ma.businessinventory.db.entity.ProductEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
-//@Database(entities = [ProductEntity::class], version = 1)
-//abstract class AppDatabase : RoomDatabase() {
-//    abstract fun productDao(): ProductDao
-//}
-
-@Database(entities = arrayOf(ProductEntity::class), version = 1)
+@Database(entities = [ProductEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun productDao(): ProductDao
 
     companion object {
+        @Volatile
         private var INSTANCE: AppDatabase? = null
-        fun getInstance(context: Context): AppDatabase {
-            if (INSTANCE == null) {
-                INSTANCE = Room.databaseBuilder(
-                    context,
-                    AppDatabase::class.java,
-                    "businessinventory"
-                ).build()
-            }
 
-            return INSTANCE as AppDatabase
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): AppDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "word_database"
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+
+        private class WordDatabaseCallback(private val scope: CoroutineScope) :
+            RoomDatabase.Callback() {
+            /**
+             * Override the onOpen method to populate the database.
+             * For this sample, we clear the database every time it is created or opened.
+             */
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                // If you want to keep the data through app restarts,
+                // comment out the following line.
+                INSTANCE?.let { database ->
+                    Log.d("********", "")
+//                    scope.launch(Dispatchers.IO) {
+//                        populateDatabase(database.productDao())
+//                    }
+                }
+            }
+        }
+
+        /**
+         * Populate the database in a new coroutine.
+         * If you want to start with more words, just add them.
+         */
+        suspend fun populateDatabase(productDao: ProductDao) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            productDao.deleteAll()
+
+//            var word = Word("Hello")
+//            wordDao.insert(word)
+//            word = Word("World!")
+//            wordDao.insert(word)
         }
     }
 }
