04d19b1a9c96721acf7498d3ef27b0565c03376f
Marcelo <marcelo.agudelo@gmail.com>
2020-01-28 10:18:24 -0500

    Fix entity

--- app/src/main/java/com/ma/businessinventory/db/entity/ProductEntity.kt
+++ app/src/main/java/com/ma/businessinventory/db/entity/ProductEntity.kt
@@ -7,18 +7,18 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "product")
 data class ProductEntity(
     @PrimaryKey(autoGenerate = true) val id: Int?,
-    @ColumnInfo(name = "name_inventory") val nameInventory: String?,
-    @ColumnInfo(name = "place") val place: String,
-    @ColumnInfo(name = "description") val description: String?,
-    @ColumnInfo(name = "type") val type: String?,
-    @ColumnInfo(name = "date") val date: String?,
-    @ColumnInfo(name = "barcode") val barcode: String?,
-    @ColumnInfo(name = "bought_no") val boughtNo: Double?,
-    @ColumnInfo(name = "sold_no") val soldNo: Double?,
-    @ColumnInfo(name = "unid_buy_price_us") val unidBuyPriceUS: Int?,
-    @ColumnInfo(name = "unid_sell_price_us") val unidSellPriceUS: Int?,
-    @ColumnInfo(name = "total_cost_us") val totalCostUS: Double?,
-    @ColumnInfo(name = "total_received_us") val totalReceivedUS: Int?,
-    @ColumnInfo(name = "total_profit_us") val totalProfitUS: Int?,
-    @ColumnInfo(name = "photo") val photo: String?
+    @ColumnInfo(name = "name_inventory") var nameInventory: String?,
+    @ColumnInfo(name = "place") var place: String,
+    @ColumnInfo(name = "description") var description: String?,
+    @ColumnInfo(name = "type") var type: String?,
+    @ColumnInfo(name = "date") var date: String?,
+    @ColumnInfo(name = "barcode") var barcode: String?,
+    @ColumnInfo(name = "bought_no") var boughtNo: Double?,
+    @ColumnInfo(name = "sold_no") var soldNo: Double?,
+    @ColumnInfo(name = "unid_buy_price_us") var unidBuyPriceUS: Int?,
+    @ColumnInfo(name = "unid_sell_price_us") var unidSellPriceUS: Int?,
+    @ColumnInfo(name = "total_cost_us") var totalCostUS: Double?,
+    @ColumnInfo(name = "total_received_us") var totalReceivedUS: Int?,
+    @ColumnInfo(name = "total_profit_us") var totalProfitUS: Int?,
+    @ColumnInfo(name = "photo") var photo: String?
 )
