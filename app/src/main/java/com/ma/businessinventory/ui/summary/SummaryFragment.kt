43166c66b767212e79e0750f51e62bc75287f3a3
Marcelo <marcelo.agudelo@gmail.com>
2020-01-29 13:58:08 -0500

    Clean code

--- app/src/main/java/com/ma/businessinventory/ui/summary/SummaryFragment.kt
+++ app/src/main/java/com/ma/businessinventory/ui/summary/SummaryFragment.kt
@@ -6,9 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.ma.businessinventory.MyApplication
 import com.ma.businessinventory.R
-import com.ma.businessinventory.db.entity.ProductEntity
 
 /**
  *
@@ -28,23 +26,7 @@ class SummaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_summary, container, false)
-
-        btnAdd = view.findViewById(R.id.btnAdd)
-
-        btnAdd.setOnClickListener {
-
-            val product =
-                ProductEntity(
-                    null, "Hello1", "Place1", "Description1", "Type1", "2020-01-22",
-                    "", 10.0, 10.0, 5, 10,
-                    15.0, 10, 10, ""
-                )
-            (activity!!.application as MyApplication).productViewModel.insert(product)
-
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_summary, container, false)
     }
 
 }
